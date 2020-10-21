62dd6845f1669726b2d6aff7e3321583e8c97f71
ldanielcc <31603194+ldanielcc@users.noreply.github.com>
2020-10-20 18:34:53 -0700

    Add files via upload

---
+++ Linux/system.sh
@@ -0,0 +1,6 @@
+
+    #!/bin/bash
+    free -h > /home/sysadmin/backups/freemem/free_mem.txt
+    du -h > /home/sysadmin/backups/diskuse/disk_usage.txt
+    lsof > /home/sysadmin/backups/openlist/open_list.txt
+    df -h > /home/sysadmin/backups/freedisk/free_disk.txt
